f66d6addc145dd7c1b6dda880e651ca5ad1b0c75
Roshan Nizar <roshanniza@inexisconsulting.com>
2020-07-23 13:21:01 +0530

    refactoring score

--- getAnswer.py
+++ getAnswer.py
@@ -43,9 +43,9 @@ def answerPredictor(a, q):
     for idx, row in data.iterrows():
         if q == row["Question"]:
             score = ratio(row["Answer"], a)
-            if score >= 0.9:  # I'm sure, stop here
+            if score >= 0.9:
                 return row["Answer"], score, row["Question"], status
-            elif score > max_score:  # I'm unsure, continue
+            elif score > max_score:
                 max_score = score
                 status = "Correct"
                 answer = row["Answer"]
@@ -55,7 +55,7 @@ def answerPredictor(a, q):
             max_score = 0
             status = "Cannot initialize"
 
-    if max_score < 0.8:  # threshold is lowered
+    if max_score < 0.8:
         status = "Wrong"
         return answer, max_score, prediction, status
     return "Sorry, I didn't get you.", max_score, prediction, status
